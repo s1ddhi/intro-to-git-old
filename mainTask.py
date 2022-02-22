f3edc9cdec5dedc13ce86d86829875e55bf67be8
Siddhi Aphiphunya <siddhi@hey.com>
2022-02-22 00:01:41 +0000

    feat: add initial task

---
+++ mainTask.py
@@ -0,0 +1,14 @@
+# With each task, don't forget to commit!
+
+# 1. Create a function that adds 2 numbers together
+def addTwo(x, y):
+    pass
+
+# 2. Create a function that prints out the input along with a fixed string
+def augmentedPrint(input):
+    fixedString = " :)"
+    pass
+
+# 3. On a separate branch called "secretFeature", complete this new function that will take 2 numbers and a string and print the result and augmented string from above altogether
+def secretMethod(x, y, input):
+    pass

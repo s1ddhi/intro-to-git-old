e52e93455bb044d2621dcee73e71b595b1e03e41
Siddhi Aphiphunya <siddhi@hey.com>
2022-02-22 00:10:25 +0000

    refactor: use tabs instead of spaces to indent

--- Task 1/mainTask.py
+++ Task 1/mainTask.py
@@ -2,13 +2,13 @@
 
 # 1. Create a function that adds 2 numbers together
 def addTwo(x, y):
-    pass
+	pass
 
 # 2. Create a function that prints out the input along with a fixed string
 def augmentedPrint(input):
-    fixedString = " :)"
-    pass
+	fixedString = " :)"
+	pass
 
 # 3. On a separate branch called "secretFeature", complete this new function that will take 2 numbers and a string and print the result and augmented string from above altogether
 def secretMethod(x, y, input):
-    pass
+	pass

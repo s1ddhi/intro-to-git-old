eb8f9bcdef4232ec57d0429fa863e08ad0f4f089
Siddhi Aphiphunya <siddhi@hey.com>
2022-02-22 04:54:24 +0000

    refactor: reword instructions in task1

--- task1/mainTask.py
+++ task1/mainTask.py
@@ -1,14 +1,14 @@
 # With each task, don't forget to commit!
 
-# 1. Create a function that adds 2 numbers together
+# 1. Complete the following function that adds 2 numbers together
 def addTwo(x, y):
 	pass
 
-# 2. Create a function that prints out the input along with a fixed string
-def augmentedPrint(input):
+# 2. Complete the following function that returns out the input string along with a fixed string
+def augmentedString(input):
 	fixedString = " :)"
 	pass
 
-# 3. On a separate branch called "secretFeature", complete this new function that will take 2 numbers and a string and print the result and augmented string from above altogether
+# 3. On a separate branch called "secretFeature", complete this new function that will take 2 numbers and a string and returns the result and augmented string from the above altogether
 def secretMethod(x, y, input):
 	pass

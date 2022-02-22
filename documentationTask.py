e87eb110088a850cb2a9d2ada5166eaa55d75449
Siddhi Aphiphunya <siddhi@hey.com>
2022-02-22 00:05:17 +0000

    feat: add documentation task

---
+++ documentationTask.py
@@ -0,0 +1,12 @@
+# TODO: Document me please!
+def addTwo(x, y):
+    return x + y
+
+# TODO: Document me please!
+def augmentedPrint(input):
+    fixedString = " :)"
+    return input + fixedString
+
+# TODO: Document me please!
+def secretMethod(x, y, input):
+    return str(addTwo(x, y)) + " " + augmentedPrint(input)
